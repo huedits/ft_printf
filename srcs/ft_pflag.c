fc1b34755da8abff11352255408f69a133db09af
vimatheu <vimatheu@student.42sp.org.br>
2022-09-20 23:24:17 +0200

    Automated commit from Makefile

--- srcs/ft_pflag.c
+++ srcs/ft_pflag.c
@@ -6,7 +6,7 @@
 /*   By: vimatheu <vimatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 21:29:32 by vimatheu          #+#    #+#             */
-/*   Updated: 2022/09/20 21:42:24 by vimatheu         ###   ########.fr       */
+/*   Updated: 2022/09/20 23:24:01 by vimatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_pflag(unsigned int nbr)
 	int	count;
 	
 	if (!nbr)
-		return(ft_putstr("(nil)"));
+		return(ft_putstr("(null)"));
 	count = ft_putstr("0x");
 	count += ft_printhex(nbr, 'x');
 	return (count);
